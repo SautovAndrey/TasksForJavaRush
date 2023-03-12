96c7ba87d38f067b5d8a938ce0ed04943c5549e1
Admin-PC <seeee8212@gmail.com>
2023-03-12 23:05:52 +0300

    Initial Commit1

--- src/MultithreadedCalculator/Main.java
+++ src/MultithreadedCalculator/Main.java
@@ -3,15 +3,15 @@ package MultithreadedCalculator;
 import java.util.ArrayList;
 
 public class Main {
-public static void main(String[] args) {
-View view = new View();
-TaskManager taskManager = new TaskManager(new ArrayList<>());
+    public static void main(String[] args) {
+        View view = new View();
+        TaskManager taskManager = new TaskManager(new ArrayList<>());
 
 // Создаем объект Controller с объектами View и TaskManager
-Controller controller = new Controller(view, taskManager);
+        Controller controller = new Controller(view, taskManager);
 
 // Запускаем контроллер
-controller.run();
-}
+        controller.run();
+    }
 }
 
